50a2cf3c61a6e0272ffe73c36f29d57265c6680a
dgsim <dgsim126@gmail.com>
2025-02-01 19:38:20 +0900

    250201

--- lv3/암기.py
+++ lv3/암기.py
@@ -1,3 +1,7 @@
 # gpt's help(key, value를 기준으로 dict 정렬하는 법 외워둘 것)
 dic = {"apple": 3, "banana": 1, "cherry": 4, "date": 2}
 new_dic= dict(sorted(dic.items(), key=lambda item: item[1], reverse=True))
+
+
+# 무인도 여행 bfs
+# 호텔 대신 최소힙
